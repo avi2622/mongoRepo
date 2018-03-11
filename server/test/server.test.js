40b5f29a2c0309df8b5e1fbeda4de08d769c9ad6
unknown <avinash.singh@atos.net>
2018-03-12 01:38:26 +0530

    test case for /todo/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,14 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
 
+var {ObjectID} = require('mongodb');
+
 var {app} = require('./../server');
 
 var {todo} = require('./../models/todo');
 
 var todos = [{
+    _id: new ObjectID(),
     text:'First Test Todo'
 },
 {
+    _id: new ObjectID(),
     text:'Second Test Todo'
 }];
 
@@ -70,6 +74,34 @@ describe('GET /todos',()=>{
         .expect((res)=>{
             expect(res.body.todos.length.toBe(2))
         })
+        .end(done);
+    });
+});
+
+describe('GET /todos/:id',()=>{
+    it('should return todo doc',(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.HexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text)
+        })
+        .end(done);
+    });
+
+    it('should return 404 if todo not found', (done)=>{
+        var hexID = new ObjectID().toHexString();
+        
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done)
+    });
+
+    it('should return 404 for non-object Ids',()=>{
+        request(app)
+        .get('/todos/123abc')
+        .expect(404)
         .end(done);
     });
 });
